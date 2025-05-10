cbb2fbfdb15d7ecd7653c474f817388c69a45077
unknown <nhloniphom397@gmail.com>
2025-05-10 05:57:41 +0200

    added firebaseConfig and modified package.json

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCX1HNNE9xPWpBIyc_Ek8AjA97Ri8GwElY",
+  authDomain: "financialapp-28e3d.firebaseapp.com",
+  projectId: "financialapp-28e3d",
+  storageBucket: "financialapp-28e3d.firebasestorage.app",
+  messagingSenderId: "210423189006",
+  appId: "1:210423189006:web:dd861161646dd3e638f65f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
